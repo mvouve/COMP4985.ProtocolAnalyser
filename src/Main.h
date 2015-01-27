33326aeba08aedf78cb20a873383a04a416ca5c2
Marc Vouve <mvouve@gmail.com>
2015-01-26 21:47:33 -0800

    Added Assignment 1 as a start point

---
+++ src/Main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/* Windows Defines */
+#include <Windows.h>
+#include <windowsx.h>
+
+void InstatiateWindow(LPTSTR name, int nCmdShow);
+
+static TCHAR NAME[] = "IP Analyser";
+HINSTANCE hInstance;
+
+#endif
